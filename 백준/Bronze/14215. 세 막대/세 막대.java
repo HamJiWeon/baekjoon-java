250962a7cb2613d3dca2f8506bedf051aca32860
HamJiWon <129733725+HamJiWeon@users.noreply.github.com>
2025-05-15 22:50:40 +0900

    [Bronze III] Title: 세 막대, Time: 172 ms, Memory: 17816 KB -BaekjoonHub

---
+++ 백준/Bronze/14215. 세 막대/세 막대.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] sticks = new int[3];
+        for (int i = 0; i < 3; i++) {
+            sticks[i] = sc.nextInt();
+        }
+
+        Arrays.sort(sticks);
+
+        if (sticks[0] + sticks[1] <= sticks[2]) {
+            sticks[2] = sticks[0] + sticks[1] - 1;
+        }
+
+        int meter = sticks[0] + sticks[1] + sticks[2];
+        System.out.println(meter);
+    }
+}

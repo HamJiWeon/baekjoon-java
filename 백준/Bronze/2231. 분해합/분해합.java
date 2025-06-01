46fb72375ec655dc073224aa102295651d595546
HamJiWon <129733725+HamJiWeon@users.noreply.github.com>
2025-06-01 22:55:45 +0900

    [Bronze II] Title: 분해합, Time: 204 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int result = 0;
+
+        for (int i = 1; i < N; i++) {
+            int sum = i;
+            int temp = i;
+
+            while (temp > 0) {
+                sum += temp % 10;
+                temp /= 10;
+            }
+
+            if (sum == N) {
+                result = i;
+                break;
+            }
+        }
+
+        System.out.println(result);
+    }
+}

c8f35fd7cd6169505942e8546e1815df9a60755b
HamJiWon <129733725+HamJiWeon@users.noreply.github.com>
2025-06-11 22:22:15 +0900

    [Bronze II] Title: 대표값2, Time: 172 ms, Memory: 17676 KB -BaekjoonHub

--- 백준/Bronze/2587. 대표값2/대표값2.java
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -6,18 +6,14 @@ class Main {
         Scanner sc = new Scanner(System.in);
         
         int[] arr = new int[5];
+        int sum = 0;
         
         for (int i = 0; i < 5; i++) {
             arr[i] = sc.nextInt();
+            sum += arr[i];
         }
         
         Arrays.sort(arr);
-        
-        int sum = 0;
-        
-        for (int arrNum : arr) {
-            sum += arrNum;
-        }
         
         System.out.println(sum / 5);
         System.out.println(arr[2]);

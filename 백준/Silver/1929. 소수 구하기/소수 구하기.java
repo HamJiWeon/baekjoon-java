aa03c23d4a1490c4e07be1a875de2217acb64a15
HamJiWon <129733725+HamJiWeon@users.noreply.github.com>
2025-07-09 17:46:33 +0900

    [Silver III] Title: 소수 구하기, Time: 800 ms, Memory: 31776 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int M = sc.nextInt();
+        int N = sc.nextInt();
+        
+        for (int i = M; i <= N; i++) {
+            if (isPrime(i)) System.out.println(i);
+        }
+    }
+    
+    public static boolean isPrime(int n) {
+        if (n < 2) return false;
+        if (n == 2) return true;
+        if (n % 2 == 0) return false;
+        
+        for (int i = 3; i * i <= n; i += 2) {
+            if (n % i == 0) return false;
+        }
+        
+        return true;
+    }
+}

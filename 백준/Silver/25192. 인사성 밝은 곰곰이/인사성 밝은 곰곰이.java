23c872bba438c28c56afe57334aa8f6d910ea3b4
HamJiWon <129733725+HamJiWeon@users.noreply.github.com>
2025-08-04 12:24:39 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 252 ms, Memory: 25640 KB -BaekjoonHub

---
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+        Set<String> greeted = new HashSet<>();
+        int count = 0;
+        
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            if (line.equals("ENTER")) greeted.clear();
+            else {
+                if (!greeted.contains(line)) {
+                    count++;
+                    greeted.add(line);
+                }
+            }
+        }
+        
+        System.out.println(count);
+    }
+}

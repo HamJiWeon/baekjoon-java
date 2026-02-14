f47381f3732e03e34fef383989c7f7012d21c941
HamJiWon <129733725+HamJiWeon@users.noreply.github.com>
2026-02-14 17:16:04 +0900

    [level 1] Title: 삼총사, Time: 0.53 ms, Memory: 88.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -0,0 +1,18 @@
+class Solution {
+    static int answer = 0;
+    
+    public int solution(int[] number) {
+        answer = 0;
+        DFS(number, 0, 0, 0);
+
+        return answer;
+    }
+
+    public void DFS(int[] number, int trio, int start, int sum) {
+        if (trio == 3 && sum == 0) answer++;
+
+        for (int i = start; i < number.length; i++) {
+            DFS(number, trio + 1, i + 1, sum + number[i]);
+        }
+    }
+}

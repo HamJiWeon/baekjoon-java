7b37d4bda520fc739d8899fcd2ddb50b22892cfb
HamJiWon <129733725+HamJiWeon@users.noreply.github.com>
2026-03-17 14:26:53 +0900

    [level 2] Title: 짝지어 제거하기, Time: 66.49 ms, Memory: 66.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        Deque<Character> dq = new ArrayDeque<>();
+        
+        for (char c : s.toCharArray()) {
+            if (dq.isEmpty() || dq.peek() != c) {
+                dq.push(c);
+            } 
+            else {
+                dq.pop();
+            }
+        }
+
+        return dq.isEmpty() ? 1 : 0;
+    }
+}

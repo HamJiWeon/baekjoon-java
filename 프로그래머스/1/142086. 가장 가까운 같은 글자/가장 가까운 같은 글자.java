b4d2b389b02bee921418b040a157a90f28df22de
HamJiWon <129733725+HamJiWeon@users.noreply.github.com>
2026-03-22 00:26:32 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 0.71 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -3,20 +3,19 @@ import java.util.*;
 class Solution {
     public int[] solution(String s) {
         int[] answer = new int[s.length()];
-        int[] lastPos = new int[26];
-        Arrays.fill(lastPos, -1);
+        int[] pos = new int[26];
+        Arrays.fill(pos, -1);
         
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             int idx = c - 'a';
             
-            if (lastPos[idx] == -1) answer[i] = -1;
-            else answer[i] = i - lastPos[idx];
+            if (pos[idx] == -1) answer[i] = -1;
+            else answer[i] = i - pos[idx];
             
-            lastPos[idx] = i;
+            pos[idx] = i;
         }
         
         return answer;
     }
 }
-

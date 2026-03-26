b56e95395594e6b9bce05f536418ee75f2fad147
HamJiWon <129733725+HamJiWeon@users.noreply.github.com>
2026-03-26 16:59:47 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.57 ms, Memory: 86.3 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -2,24 +2,23 @@ import java.util.*;
 
 class Solution {
     public int solution(String s) {
-
-        HashMap<String, String> number = new HashMap<>();
+        String[] arr = {
+            "zero",
+            "one",
+            "two",
+            "three",
+            "four",
+            "five",
+            "six",
+            "seven",
+            "eight",
+            "nine"
+        };
         
-        number.put("zero", "0");
-        number.put("one", "1");
-        number.put("two", "2");
-        number.put("three", "3");
-        number.put("four", "4");
-        number.put("five", "5");
-        number.put("six", "6");
-        number.put("seven", "7");
-        number.put("eight", "8");
-        number.put("nine", "9");
-
-        for (String key : number.keySet()) {
-            s = s.replaceAll(key, number.get(key));
+        for (int i = 0; i < arr.length; i++) {
+            s = s.replaceAll(arr[i], String.valueOf(i));
         }
-
+        
         return Integer.parseInt(s);
     }
 }

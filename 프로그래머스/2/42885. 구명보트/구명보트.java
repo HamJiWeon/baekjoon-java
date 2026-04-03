a8ba406bb63092872573a44f7b59773d5c0f94a6
HamJiWon <129733725+HamJiWeon@users.noreply.github.com>
2026-04-03 18:58:57 +0900

    [level 2] Title: 구명보트, Time: 9.97 ms, Memory: 55.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        
+        Arrays.sort(people);
+        
+        int lt = 0, rt = people.length - 1;
+        while (lt < rt) {
+            if (people[lt] + people[rt] <= limit) {
+                lt++;
+            }
+            
+            rt--;
+            answer++;
+        }
+        
+        if (rt == lt) answer++;
+        
+        return answer;
+    }
+}

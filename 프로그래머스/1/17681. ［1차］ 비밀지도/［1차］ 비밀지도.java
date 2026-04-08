24bc8c1c58e8788f1e04280d4bbe72e2ea6dbba9
HamJiWon <129733725+HamJiWeon@users.noreply.github.com>
2026-04-08 15:58:38 +0900

    [level 1] Title: [1차] 비밀지도, Time: 16.09 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,24 +1,16 @@
-import java.util.*;
-
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         
         String[] answer = new String[n];
-        
+
         for (int i = 0; i < n; i++) {
-            String s = Integer.toBinaryString(arr1[i] | arr2[i]);
-            
+            String s = String.format("%" + n + "s", Integer.toBinaryString(arr1[i] | arr2[i]));
+
             while (s.length() < n) s = "0" + s;
-            
-            StringBuilder sb = new StringBuilder();
-            
-            for (int j = 0; j < n; j++) {
-                sb.append(s.charAt(j) == '1' ? "#" : " ");
-            }
-            
-            answer[i] = sb.toString();
+
+            answer[i] = s.replace('0', ' ').replace('1', '#');
         }
-        
+
         return answer;
     }
 }

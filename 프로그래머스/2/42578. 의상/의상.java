1c7b13a3f725ff03d63a72a3ed9d8d160be11079
HamJiWon <129733725+HamJiWeon@users.noreply.github.com>
2026-04-18 19:25:39 +0900

    [level 2] Title: 의상, Time: 0.42 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -3,15 +3,15 @@ import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
         int answer = 1;
-        
+
         Map<String, Integer> map = new HashMap<>();
-        
+
         for (String[] c : clothes) {
-            map.put(c[1], map.getOrDefault(c[1], 0) + 1);
+            map.merge(c[1], 1, (a, b) -> a + b);
         }
-        
+
         for (int v : map.values()) {
-            answer *= (v + 1);
+            answer *= v + 1;
         }
         
         return answer - 1;

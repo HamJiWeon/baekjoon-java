eb5b0330f71976a90976a04e5f02e5c81d21ff6a
HamJiWon <129733725+HamJiWeon@users.noreply.github.com>
2026-04-19 15:52:58 +0900

    [level 2] Title: H-Index, Time: 8.85 ms, Memory: 84.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        
+        citations = Arrays.stream(citations)
+            .boxed()
+            .sorted(Collections.reverseOrder())
+            .mapToInt(Integer::intValue)
+            .toArray();
+        
+        for (int i = 0; i < citations.length; i++) {
+            if (citations[i] >= i + 1) answer = i + 1;
+            else break;
+        }
+        
+        
+        
+        return answer;
+    }
+}

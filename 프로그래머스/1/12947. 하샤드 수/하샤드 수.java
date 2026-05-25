4623ddddc969c5ab5dc1399878c6830ccc9b1c33
HamJiWon <129733725+HamJiWeon@users.noreply.github.com>
2026-05-25 18:45:08 +0900

    [level 1] Title: 하샤드 수, Time: 1.07 ms, Memory: 60.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        
+        int sum = String.valueOf(x)
+            .chars()
+            .map(Character::getNumericValue)
+            .sum();
+        
+        return x % sum == 0;
+    }
+}

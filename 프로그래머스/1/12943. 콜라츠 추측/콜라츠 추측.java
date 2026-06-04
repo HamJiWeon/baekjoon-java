4f3595a51cc662d173c2e39706ef9917f121f2d3
HamJiWon <129733725+HamJiWeon@users.noreply.github.com>
2026-06-04 10:50:56 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 65.1 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -2,19 +2,18 @@ class Solution {
     public int solution(int num) {
         int answer = 0;
         
-        long n = (long) num;
+        if (num == 1) return 0;
         
-        if (n == 1) return 0;
-        
-        while (n != 1) {
-            if (answer == 500) return -1;
+        while (num != 1) {
             
-            if (n % 2 == 0) n /= 2;
-            else n = (n * 3) + 1;
+            if (answer == 400) return -1;
+            
+            if (num % 2 == 0) num /= 2;
+            else num = (num * 3) + 1;
             
             answer++;
         }
-        
+            
         return answer;
     }
 }

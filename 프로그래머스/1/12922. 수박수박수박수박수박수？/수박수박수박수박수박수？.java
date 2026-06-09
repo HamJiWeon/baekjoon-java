9fa6b77a1379a925c070f7bac579d23e33afa521
HamJiWon <129733725+HamJiWeon@users.noreply.github.com>
2026-06-09 22:21:22 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.09 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,6 +1,6 @@
 class Solution {
     public String solution(int n) {
-        String answer = "수박".repeat(20000);
+        String answer = "수박".repeat(10000);
         
         String s = answer.substring(0, n);
         

7864a0190e0f649162689586d4b46476022981f7
HamJiWon <129733725+HamJiWeon@users.noreply.github.com>
2026-06-09 10:02:30 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.28 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -3,10 +3,22 @@ class Solution {
         int answer = 0;
         
         for (int i = left; i <= right; i++) {
-            if (Math.sqrt(i) % 1 == 0) answer -= i;
-            else answer += i;
+            answer += (divisorCnt(i) % 2 == 0) ? i : -i; 
         }
         
         return answer;
+    }
+    
+    public int divisorCnt(int num) {
+        
+        int cnt = 0;
+        for (int i = 1; i * i <= num; i++) {
+            if (num % i == 0) {
+                if (i * i == num) cnt++;
+                else cnt += 2;
+            }
+        }
+        
+        return cnt;
     }
 }

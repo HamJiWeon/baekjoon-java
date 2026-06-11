66169327873dcfbab0b273e6be790f5b507ee5f9
HamJiWon <129733725+HamJiWeon@users.noreply.github.com>
2026-06-11 16:37:56 +0900

    [level 1] Title: 행렬의 덧셈, Time: 2.37 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.java
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.java
@@ -1,15 +1,13 @@
 class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr1[0].length];
         
-        int rows = arr1.length;
-        int cols = arr1[0].length;
-        
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                arr1[i][j] += arr2[i][j];
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1[0].length; j++) {
+                answer[i][j] = arr1[i][j] + arr2[i][j];
             }
         }
         
-        return arr1;
+        return answer;
     }
 }

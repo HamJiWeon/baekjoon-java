bc732aacead4341e316efbef1053b75ba382aaf1
HamJiWon <129733725+HamJiWeon@users.noreply.github.com>
2026-06-23 13:36:33 +0900

    [level 2] Title: 피보나치 수, Time: 1.34 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,16 +1,19 @@
 class Solution {
     public int solution(int n) {
-        if (n == 0) return 0;
-        if (n == 1) return 1;
-
-        int[] dp = new int[n + 1];
-        dp[0] = 0;
-        dp[1] = 1;
-
-        for (int i = 2; i <= n; i++) {
-            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+        int answer = 0;
+        // 1 1 2 3 5 8 13 21
+        
+        int prev1 = 1;
+        int prev2 = 1;
+        
+        if (n == 2) return 1;
+        
+        for (int i = 3; i <= n; i++) {
+            answer = (prev1 + prev2) % 1234567;
+            prev2 = prev1;
+            prev1 = answer;
         }
-
-        return dp[n];
+        
+        return answer;
     }
 }
